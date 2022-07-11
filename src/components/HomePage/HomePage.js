96317663d0ec509d6823321df601fc02facf67e2
masontaylor7 <masonevantaylor@gmail.com>
2022-07-10 22:11:36 -0700

    first official commit, profile image and bio added with some css styling for light/dark modes

---
+++ src/components/HomePage/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './HomePage.css'
+import NavBar from '../NavBar/NavBar';
+
+const HomePage = () => {
+    return (
+        <div className='home-page'>
+            <NavBar />
+        </div>
+    );
+};
+
+export default HomePage;
---
+++ src/data/mydata.js
@@ -0,0 +1,48 @@
+export const myself = {
+    'info': {
+        'firstName': 'Mason',
+        'lastName': 'Taylor',
+        'middleName': 'Evan',
+        'email': 'masonevantaylor@gmail.com',
+        'linkedin': 'linkedin.com/in/mtaylor77/',
+        'github': 'https://github.com/masontaylor7',
+    },
+    'projects': {
+        'alacarte': {
+            'id': 1,
+            'image': '',
+            'title': 'Alacarte',
+            'technologies': [
+                'React.js',
+                'Redux',
+                'Python',
+                'Flask',
+                'SQLAlchemy',
+                'AWS',
+                'HTML5',
+                'CSS3'
+            ],
+            'description': 'A full-stack application where users can browse recipes, add recipes, and create collections to save their favorite recipes.',
+            'liveSite': 'https://ala-carte.herokuapp.com/',
+            'github': 'https://github.com/masontaylor7/Alacarte',
+        },
+        'flixtagram': {
+            'id': 2,
+            'image': '',
+            'title': 'Flixtagram',
+            'technologies': [
+                'React.js',
+                'Redux',
+                'Python',
+                'Flask',
+                'PostgreSQL',
+                'AWS',
+                'HTML5',
+                'CSS3'
+            ],
+            'description': 'A full-stack Instagram clone where users can upload photos, like photos, and follow other users.',
+            'liveSite': 'https://flixtagram.herokuapp.com/',
+            'github': 'https://github.com/parth610/Insta-Clone',
+        }
+    },
+    }
